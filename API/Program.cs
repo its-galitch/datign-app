55c6142858a40193d4e7037a1d5bb853d6e2675c
Yitzhak Galitch <its.galitch@gmail.com>
2025-02-16 14:24:33 +0200

    Errors handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 var allowedOrigins = new[] { "http://localhost:4200", "https://localhost:4200" };
 app.UseCors( x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins(allowedOrigins));
---
+++ DatingClient/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,56 @@
+import {Component, inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'da-test-errors',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-errors.component.html',
+  styleUrl: './test-errors.component.scss'
+})
+export class TestErrorsComponent {
+  baseUrl = 'https://localhost:5001/api/';
+  #http = inject(HttpClient);
+  validationErrors: string[] = [];
+
+  get400Error() {
+    this.#http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+  get401Error() {
+    this.#http.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+  get404Error() {
+    this.#http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+  get500Error() {
+    this.#http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    });
+  }
+
+
+  get400ValidationError() {
+    this.#http.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: response => console.log(response),
+      error: errors => {
+        console.log(errors);
+        this.validationErrors = errors;
+      }
+    });
+  }
+
+
+}
